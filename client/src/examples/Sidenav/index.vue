97a93e1599220963cc71fe3a74084875649e0a4c
yk-mt12 <agvksl543@gmail.com>
2022-09-15 14:24:49 +0000

    フォーマットを修正

--- client/src/examples/Sidenav/index.vue
+++ client/src/examples/Sidenav/index.vue
@@ -1,12 +1,6 @@
 <template>
-  <div
-    v-show="this.$store.state.layout === 'default'"
-    class="min-height-300 position-absolute w-100"
-    :class="`${this.$store.state.darkMode ? 'bg-transparent' : 'bg-success'}`"
-  />
-  <aside
-    class="my-3 overflow-auto border-0 sidenav navbar navbar-vertical navbar-expand-xs border-radius-xl"
-    :class="`${
+  <div v-show="this.$store.state.layout === 'default'" class="min-height-300 position-absolute w-100" :class="`${this.$store.state.darkMode ? 'bg-transparent' : 'bg-success'}`" />
+  <aside class="my-3 overflow-auto border-0 sidenav navbar navbar-vertical navbar-expand-xs border-radius-xl" :class="`${
       this.$store.state.isRTL
         ? 'me-3 rotate-caret fixed-end'
         : 'fixed-start ms-3'
@@ -15,17 +9,11 @@
       this.$store.state.layout === 'landing'
         ? 'bg-transparent shadow-none'
         : ' '
-    } ${this.$store.state.sidebarType}`"
-    id="sidenav-main"
-  >
+    } ${this.$store.state.sidebarType}`" id="sidenav-main">
     <div class="sidenav-header">
-      <i
-        class="top-0 p-3 cursor-pointer fas fa-times text-secondary opacity-5 position-absolute end-0 d-none d-xl-none"
-        aria-hidden="true"
-        id="iconSidenav"
-      ></i>
+      <i class="top-0 p-3 cursor-pointer fas fa-times text-secondary opacity-5 position-absolute end-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <router-link class="m-0 navbar-brand" to="/">
-        
+
         <h3 class="ms-2 font-weight-bold me-2">ALCoho</h3>
       </router-link>
     </div>
@@ -34,21 +22,21 @@
   </aside>
 </template>
 <script>
-import SidenavList from "./SidenavList.vue";
-import logo from "@/assets/img/logo-ct-dark.png";
-import logoWhite from "@/assets/img/logo-ct.png";
+  import SidenavList from "./SidenavList.vue";
+  import logo from "@/assets/img/logo-ct-dark.png";
+  import logoWhite from "@/assets/img/logo-ct.png";
 
-export default {
-  name: "index",
-  components: {
-    SidenavList
-  },
-  data() {
-    return {
-      logo,
-      logoWhite
-    };
-  },
-  props: ["custom_class", "layout"]
-};
+  export default {
+    name: "index",
+    components: {
+      SidenavList
+    },
+    data() {
+      return {
+        logo,
+        logoWhite
+      };
+    },
+    props: ["custom_class", "layout"]
+  };
 </script>
