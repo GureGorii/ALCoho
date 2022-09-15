5aff867e63794b0add3b130c04a7298e5f3a2351
yk-mt12 <agvksl543@gmail.com>
2022-09-15 07:52:18 +0000

    サインインボタンを作成

--- client/src/views/Signin.vue
+++ client/src/views/Signin.vue
@@ -2,11 +2,7 @@
   <div class="container top-0 position-sticky z-index-sticky">
     <div class="row">
       <div class="col-12">
-        <navbar
-          isBlur="blur  border-radius-lg my-3 py-2 start-0 end-0 mx-4 shadow"
-          v-bind:darkMode="true"
-          isBtn="bg-gradient-success"
-        />
+        <navbar isBlur="blur  border-radius-lg my-3 py-2 start-0 end-0 mx-4 shadow" v-bind:darkMode="true" isBtn="bg-gradient-success" />
       </div>
     </div>
   </div>
@@ -22,52 +18,32 @@
                   <p class="mb-0">Enter your email and password to sign in</p>
                 </div>
                 <div class="card-body">
-                  <form role="form">
+                  <form role="form" @submit.prevent="submit">
                     <div class="mb-3">
-                      <argon-input type="email" placeholder="Email" name="email" size="lg" />
+                      <input type="text" placeholder="userId" name="userId" size="lg" v-model="user.userId" class="input-form"/>
                     </div>
                     <div class="mb-3">
-                      <argon-input type="password" placeholder="Password" name="password" size="lg" />
+                      <input type="password" placeholder="Password" name="password" size="lg" v-model="user.password" class="input-form"/>
                     </div>
-                    <argon-switch id="rememberMe">Remember me</argon-switch>
-
                     <div class="text-center">
-                      <argon-button
-                        class="mt-4"
-                        variant="gradient"
-                        color="success"
-                        fullWidth
-                        size="lg"
-                      >Sign in</argon-button>
+                      <argon-button class="mt-4" variant="gradient" color="success" fullWidth size="lg" >Sign in</argon-button>
                     </div>
                   </form>
                 </div>
                 <div class="px-1 pt-0 text-center card-footer px-lg-2">
                   <p class="mx-auto mb-4 text-sm">
                     Don't have an account?
-                    <a
-                      href="javascript:;"
-                      class="text-success text-gradient font-weight-bold"
-                    >Sign up</a>
+                    <a href="https://0bc8d0957737489bbf37971f840b3cf1.vfs.cloud9.ap-northeast-1.amazonaws.com/signup" class="text-success text-gradient font-weight-bold">Sign up</a>
                   </p>
                 </div>
               </div>
             </div>
-            <div
-              class="top-0 my-auto text-center col-6 d-lg-flex d-none h-100 pe-0 position-absolute end-0 justify-content-center flex-column"
-            >
-              <div
-                class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
-          background-size: cover;"
-              >
+            <div class="top-0 my-auto text-center col-6 d-lg-flex d-none h-100 pe-0 position-absolute end-0 justify-content-center flex-column">
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
+          background-size: cover;">
                 <span class="mask bg-gradient-success opacity-6"></span>
-                <h4
-                  class="mt-5 text-white font-weight-bolder position-relative"
-                >"Attention is the new currency"</h4>
-                <p
-                  class="text-white position-relative"
-                >The more effortless the writing looks, the more effort the writer actually put into the process.</p>
+                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
+                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
               </div>
             </div>
           </div>
@@ -78,33 +54,77 @@
 </template>
 
 <script>
-import Navbar from "@/examples/PageLayout/Navbar.vue";
-import ArgonInput from "@/components/ArgonInput.vue";
-import ArgonSwitch from "@/components/ArgonSwitch.vue";
-import ArgonButton from "@/components/ArgonButton.vue";
-const body = document.getElementsByTagName("body")[0];
+  import { baseUrl } from '@/assets/config.js';
+  import axios from "axios";
+  import Navbar from "@/examples/PageLayout/Navbar.vue";
+  import ArgonInput from "@/components/ArgonInput.vue";
+  import ArgonSwitch from "@/components/ArgonSwitch.vue";
+  import ArgonButton from "@/components/ArgonButton.vue";
+  const body = document.getElementsByTagName("body")[0];
 
-export default {
-  name: "signin",
-  components: {
-    Navbar,
-    ArgonInput,
-    ArgonSwitch,
-    ArgonButton,
-  },
-  created() {
-    this.$store.state.hideConfigButton = true;
-    this.$store.state.showNavbar = false;
-    this.$store.state.showSidenav = false;
-    this.$store.state.showFooter = false;
-    body.classList.remove("bg-gray-100");
-  },
-  beforeUnmount() {
-    this.$store.state.hideConfigButton = false;
-    this.$store.state.showNavbar = true;
-    this.$store.state.showSidenav = true;
-    this.$store.state.showFooter = true;
-    body.classList.add("bg-gray-100");
-  },
-};
+  export default {
+    name: "signin",
+    components: {
+      Navbar,
+      ArgonInput,
+      ArgonSwitch,
+      ArgonButton,
+    },
+    created() {
+      this.$store.state.hideConfigButton = true;
+      this.$store.state.showNavbar = false;
+      this.$store.state.showSidenav = false;
+      this.$store.state.showFooter = false;
+      body.classList.remove("bg-gray-100");
+    },
+    data() {
+      return {
+        user: {
+          userId: null,
+          password: null
+        }
+      }
+    },
+    methods: {
+      async submit() {
+        console.log('ボタンが押された')
+        const headers = { 'Authorization': 'mtiToken' }
+        // リクエストボディを指定する
+        const requestBody = {
+          userId: this.user.userId,
+          password: this.user.password,
+        };
+
+        try {
+          const res = await axios.post(baseUrl + '/user/login', requestBody, { headers });
+          console.log(res)
+          // 成功時の処理
+          window.localStorage.setItem('token', res.data.token);
+          window.localStorage.setItem('userId', this.user.userId)
+          this.$router.push({ name: "Dashboard",})
+          console.log('res.data: ', res.data)
+        }
+        catch (e) {
+          // エラー時の処理
+          alert("ログインに失敗しました。")
+        }
+
+      }
+    },
+    beforeUnmount() {
+      this.$store.state.hideConfigButton = false;
+      this.$store.state.showNavbar = true;
+      this.$store.state.showSidenav = true;
+      this.$store.state.showFooter = true;
+      body.classList.add("bg-gray-100");
+    },
+  };
 </script>
+<style scoped>
+.input-form {
+  min-width: 160px;
+  border-radius: 5px;
+  border: solid 1px;
+}
+
+</style>
