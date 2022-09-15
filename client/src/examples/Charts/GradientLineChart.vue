92c01280ce3e677fac9bff690585788d4daaebb6
427brt <yu0427suzuki@icloud.com>
2022-09-15 20:44:00 +0000

    チャートとカードにデータ反映

--- client/src/examples/Charts/GradientLineChart.vue
+++ client/src/examples/Charts/GradientLineChart.vue
@@ -18,10 +18,268 @@
 
 <script>
 import Chart from "chart.js/auto";
+//import user from '@/assets/sampleData.json'; 
+
 
 
 export default {
   name: "gradient-line-chart",
+  data(){
+    return{
+      sdata:
+      {
+        id:[
+          {
+            index: 0,
+            date: 1, //
+            step: 514,  //
+            aDayCalorie: 459, //  100歩で3.5Kcal
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 1,
+            date: 2,
+            step: 14913,
+            aDayCalorie: 452,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 2,
+            date: 3,
+            step: 9786,
+            aDayCalorie: 342,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 3,
+            date: 4,
+            step: 3631,
+            aDayCalorie: 324,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 4,
+            date: 5,
+            step: 2393,
+            aDayCalorie: 506,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 5,
+            date: 6,
+            step: 6260,
+            aDayCalorie: 516,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 6,
+            date: 7,
+            step: 5933,
+            aDayCalorie: 217,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 7,
+            date: 8,
+            step: 6974,
+            aDayCalorie: 689,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 8,
+            date: 9,
+            step: 8460,
+            aDayCalorie: 526,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 9,
+            date: 10,
+            step: 6539,
+            aDayCalorie: 388,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 10,
+            date: 11,
+            step: 7187,
+            aDayCalorie: 377,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 11,
+            date: 12,
+            step: 11763,
+            aDayCalorie: 172,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 12,
+            date: 13,
+            step: 14322,
+            aDayCalorie: 404,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 13,
+            date: 14,
+            step: 881,
+            aDayCalorie: 622,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 14,
+            date: 15,
+            step: 6517,
+            aDayCalorie: 388,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 15,
+            date: 16,
+            step: 1946,
+            aDayCalorie: 233,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 16,
+            date: 17,
+            step: 8185,
+            aDayCalorie: 642,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 17,
+            date: 18,
+            step: 13227,
+            aDayCalorie: 400,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 18,
+            date: 19,
+            step: 411,
+            aDayCalorie: 428,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 19,
+            date: 20,
+            step: 9185,
+            aDayCalorie: 404,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 20,
+            date: 21,
+            step: 3340,
+            aDayCalorie: 342,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 21,
+            date: 22,
+            step: 1201,
+            aDayCalorie: 199,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 22,
+            date: 23,
+            step: 9604,
+            aDayCalorie: 491,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 23,
+            date: 24,
+            step: 8552,
+            aDayCalorie: 213,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 24,
+            date: 25,
+            step: 307,
+            aDayCalorie: 313,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 25,
+            date: 26,
+            step: 8455,
+            aDayCalorie: 393,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 26,
+            date: 27,
+            step: 9560,
+            aDayCalorie: 500,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 27,
+            date: 28,
+            step: 9376,
+            aDayCalorie: 678,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 28,
+            date: 29,
+            step: 3111,
+            aDayCalorie: 331,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 29,
+            date: 30,
+            step: 8700,
+            aDayCalorie: 603,
+            basalMetabolism: 1530
+          }
+          ]
+       
+    },
+    /* title:{
+      title: "１週間の歩数",
+      detail1: "%",
+      detail2: "前日と比べて"
+    }
+    */
+
+    };
+  },
 
   props: {
     title: {
@@ -30,18 +288,23 @@ export default {
     },
     detail1: {
       type: String,
-      default: "4% more",
+      default: '4% more',
     },
     detail2: {
       type: String,
-      default: "in 2022年9月",
+      default: "前日と比べて",
     },
   },
 
   mounted() {
     var ctx1 = document.getElementById("chart-line").getContext("2d");
-
+    const sdata = this.sdata;
+    //var status = sdata.id[num[7]] / sdata.id[num[6]] * 100;
     var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+    var num = new Array(30);
+    for(var i = 0; i<30; i++){
+      num[i] = Math.floor( Math.random() * 30 ) ;
+    };
 
     gradientStroke1.addColorStop(1, "rgba(94, 114, 228, 0.2)");
     gradientStroke1.addColorStop(0.2, "rgba(94, 114, 228, 0.0)");
@@ -61,7 +324,8 @@ export default {
             // eslint-disable-next-line no-dupe-keys
             borderWidth: 3,
             fill: true,
-            data: [50, 40, 300, 220, 500, 250, 400, 230, 500, 40, 300, 220, 500, 250, 400, 230, 500],
+            data: [sdata.id[num[1]].step, sdata.id[num[2]].step, sdata.id[num[3]].step, sdata.id[num[4]].step, sdata.id[num[5]].step, sdata.id[num[6]].step, sdata.id[num[7]].step],
+            //data: [50, 40, 300, 220, 500, 250, 400, 230, 500, 40, 300, 220, 500, 250, 400, 230, 500],
             maxBarThickness: 6,
           },
         ],
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -19,7 +19,7 @@
           <div class="col-lg-4">
             <card
               :title="stats.users.title"
-              :value="stats.users.value"
+              :value="sdata.id[Math.floor( Math.random() * 30 )].step"
               :percentage="stats.users.percentage"
               :iconClass="stats.users.iconClass"
               :iconBackground="stats.users.iconBackground"
@@ -31,7 +31,7 @@
           <div class="col-lg-4">
             <card
               :title="stats.clients.title"
-              :value="stats.clients.value"
+              :value="sdata.id[Math.floor( Math.random() * 30 )].aDayCalorie"
               :percentage="stats.clients.percentage"
               :iconClass="stats.clients.iconClass"
               :iconBackground="stats.clients.iconBackground"
@@ -136,6 +136,7 @@ let d = new Date();
 let year = d.getUTCFullYear();
 let month = d.getUTCMonth() + 1;
 let day = d.getUTCDate();
+//var num = Math.floor( Math.random() * 30 );
 
 let time = year + '年' + month + '月' + day + '日';
 export default {
@@ -144,10 +145,8 @@ export default {
     return {
       stats: {
         money: {
-
           value: time,
           iconClass: "ni ni-calendar-grid-58",
-
           iconBackground: "bg-gradient-primary",
         },
         users: {
@@ -159,7 +158,7 @@ export default {
           detail: "前週比",
         },
         clients: {
-          title: "消費カロリー",
+          title: "消費カロリー(kcal)",
           value: "3,000" + "kcal",
           percentage: "-2%",
           iconClass: "fa fa-cutlery",
@@ -216,6 +215,253 @@ export default {
 
         },
       },
+      
+      sdata:
+      {
+        id:[
+          {
+            index: 0,
+            date: 1, //
+            step: 514,  //
+            aDayCalorie: 459, //  100歩で3.5Kcal
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 1,
+            date: 2,
+            step: 14913,
+            aDayCalorie: 452,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 2,
+            date: 3,
+            step: 9786,
+            aDayCalorie: 342,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 3,
+            date: 4,
+            step: 3631,
+            aDayCalorie: 324,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 4,
+            date: 5,
+            step: 2393,
+            aDayCalorie: 506,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 5,
+            date: 6,
+            step: 6260,
+            aDayCalorie: 516,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 6,
+            date: 7,
+            step: 5933,
+            aDayCalorie: 217,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 7,
+            date: 8,
+            step: 6974,
+            aDayCalorie: 689,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 8,
+            date: 9,
+            step: 8460,
+            aDayCalorie: 526,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 9,
+            date: 10,
+            step: 6539,
+            aDayCalorie: 388,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 10,
+            date: 11,
+            step: 7187,
+            aDayCalorie: 377,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 11,
+            date: 12,
+            step: 11763,
+            aDayCalorie: 172,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 12,
+            date: 13,
+            step: 14322,
+            aDayCalorie: 404,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 13,
+            date: 14,
+            step: 881,
+            aDayCalorie: 622,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 14,
+            date: 15,
+            step: 6517,
+            aDayCalorie: 388,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 15,
+            date: 16,
+            step: 1946,
+            aDayCalorie: 233,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 16,
+            date: 17,
+            step: 8185,
+            aDayCalorie: 642,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 17,
+            date: 18,
+            step: 13227,
+            aDayCalorie: 400,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 18,
+            date: 19,
+            step: 411,
+            aDayCalorie: 428,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 19,
+            date: 20,
+            step: 9185,
+            aDayCalorie: 404,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 20,
+            date: 21,
+            step: 3340,
+            aDayCalorie: 342,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 21,
+            date: 22,
+            step: 1201,
+            aDayCalorie: 199,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 22,
+            date: 23,
+            step: 9604,
+            aDayCalorie: 491,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 23,
+            date: 24,
+            step: 8552,
+            aDayCalorie: 213,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 24,
+            date: 25,
+            step: 307,
+            aDayCalorie: 313,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 25,
+            date: 26,
+            step: 8455,
+            aDayCalorie: 393,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 26,
+            date: 27,
+            step: 9560,
+            aDayCalorie: 500,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 27,
+            date: 28,
+            step: 9376,
+            aDayCalorie: 678,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 28,
+            date: 29,
+            step: 3111,
+            aDayCalorie: 331,
+            basalMetabolism: 1530
+          },
+
+          {
+            index: 29,
+            date: 30,
+            step: 8700,
+            aDayCalorie: 603,
+            basalMetabolism: 1530
+          }
+          ]
+       
+    },
+      
     };
   },
   components: {
