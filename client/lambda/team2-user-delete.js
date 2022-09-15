92826a94b306e93c7efa962425eadfa4d136c517
Baba Seiya <kamekame7200s@yahoo.co.jp>
2022-09-15 01:06:25 +0000

    lambdaのリネームと編集

--- client/lambda/team2-user-delete.js
+++ client/lambda/team2-user-delete.js
@@ -1,7 +1,6 @@
-//team2 3記事の一件削除
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const TableName = "Article";
+const TableName = "UserInfo";
 
 exports.handler = async (event, context) => {
   const response = {
@@ -11,44 +10,31 @@ exports.handler = async (event, context) => {
     },
     body: JSON.stringify({ message: "" }),
   };
-
-  const userId = (JSON.parse(event.body)).userId;
-  const timestamp = (JSON.parse(event.body)).timestamp;
-
-  if (event.headers.authorization !== "mtiToken") {
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: "Tokenが違いますログインしてください",
-    });
-    return response;
-  }
+  
+  const userId = (JSON.parse(event.body)).userId
 
   // TODO: 削除対象のテーブル名と削除したいデータのkeyをparamに設定
   const param = {
     TableName,
-    Key: {
-      userId,
-      timestamp
+    Key:{
+      userId
     }
   };
-
-
-  try {
+  
+  
+ try{
     // dynamo.delete()を用いてデータを削除
     await dynamo.delete(param).promise();
+    response.statusCode = 204;
     // TODO: 成功後の処理を記載(status codeを指定する。)
-    response.statusCode = 204
-    response.body = JSON.stringify({
-      message: "deleted!"
-    })
-  }
-  catch (e) {
+  
+  }catch(e){
     response.statusCode = 500;
     response.body = JSON.stringify({
       message: "予期せぬエラーが発生しました。",
       errorDetail: e.toString()
     });
   }
-
+  
   return response;
 };
--- client/lambda/team2-user-signup-post.js
+++ client/lambda/team2-user-signup-post.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const TableName = "User";
+const TableName = "UserInfo";
 
 exports.handler = async (event, context) => {
   const response = {
@@ -12,16 +12,17 @@ exports.handler = async (event, context) => {
   };
 
   // TODO: リクエストボディの中身をJavaScriptオブジェクトに変換し、1つ、あるいは複数の変数に代入する
-  const { userId, age, nickname, password } = JSON.parse(event.body);
+  const { userId, password, height,weight,api } = JSON.parse(event.body);
   // TODO: DBに登録するための情報をparamオブジェクトとして宣言する（中身を記述）
   const param = {
     // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。 
     TableName,  // TableName: TableNameと同じ意味
     Item: {
       userId,   // userId: userIdと同じ意味
-      age,      // age: ageと同じ意味
-      nickname, // nickname: nicknameと同じ意味
-      password // password: passwordと同じ意味
+      height,      // age: ageと同じ意味
+      weight, // nickname: nicknameと同じ意味
+      password, // password: passwordと同じ意味
+      api
     }
   };
 
@@ -31,7 +32,7 @@ exports.handler = async (event, context) => {
     await dynamo.put(param).promise();
     // TODO: 登録に成功した場合の処理を記載する。(status codeの設定と、response bodyの設定)
     response.statusCode = 201;
-    response.body = JSON.stringify({userId, age, nickname});
+    response.body = JSON.stringify({userId, password, height,weight,api});
   }catch(e){
     response.statusCode = 500;
     response.body = JSON.stringify({
