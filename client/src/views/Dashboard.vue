7dde6fdf3ec7ac83fc9ec61a356f8335e50f658a
yk-mt12 <agvksl543@gmail.com>
2022-09-16 04:07:38 +0000

    チャートとダッシュボードの歩数をcreateNum関数によって共通化

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -119,15 +119,18 @@
   // import RegistCard from "./components/RegistCard.vue";
   import ArgonButton from "@/components/ArgonButton.vue";
   import PI from "@/assets/img/icons/flags/personicon.png";
+  import createNum from '../logic/createNum.js'
   let d = new Date();
   let year = d.getUTCFullYear();
   let month = d.getUTCMonth() + 1;
   let day = d.getUTCDate();
   let time = year + '年' + month + '月' + day + '日';
+  const numArray = createNum
   export default {
     name: "dashboard-default",
     data() {
       return {
+        num: numArray[7],
         stats: {
           money: {
             title: "日付",
@@ -153,7 +156,7 @@
             detail: "前日比",
           },
           drink: {
-            title: "今日飲んで良い量 (杯)",//こっち
+            title: "今日飲んで良い量 (杯)", //こっち
             value: "ビール" + "3" + "杯",
             percentage: "-2%",
             iconClass: "fa fa-beer",
@@ -192,265 +195,251 @@
             flag: PI,
           },
         },
-        
         sdata: {
-        id: [
-          {
-            index: 0,
-            date: 1, //
-            step: 514, //
-            aDayCalorie: (514 / 100 * 3.5).toFixed(3), //  100歩で3.5Kcal
-            basalMetabolism: 1530
+          id: [
+            {
+              index: 0,
+              date: 1, //
+              step: 514, //
+              aDayCalorie: (514 / 100 * 3.5).toFixed(3), //  100歩で3.5Kcal
+              basalMetabolism: 1530
           },
 
-          {
-            index: 1,
-            date: 2,
-            step: 14913,
-            aDayCalorie: (14913 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 1,
+              date: 2,
+              step: 14913,
+              aDayCalorie: (14913 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 2,
-            date: 3,
-            step: 9786,
-            aDayCalorie: (9786 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 2,
+              date: 3,
+              step: 9786,
+              aDayCalorie: (9786 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 3,
-            date: 4,
-            step: 3631,
-            aDayCalorie: (3631 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 3,
+              date: 4,
+              step: 3631,
+              aDayCalorie: (3631 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 4,
-            date: 5,
-            step: 2393,
-            aDayCalorie: (2393 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 4,
+              date: 5,
+              step: 2393,
+              aDayCalorie: (2393 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 5,
-            date: 6,
-            step: 6260,
-            aDayCalorie: (6260 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 5,
+              date: 6,
+              step: 6260,
+              aDayCalorie: (6260 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 6,
-            date: 7,
-            step: 5933,
-            aDayCalorie: (5933 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 6,
+              date: 7,
+              step: 5933,
+              aDayCalorie: (5933 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 7,
-            date: 8,
-            step: 6974,
-            aDayCalorie: (6974 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 7,
+              date: 8,
+              step: 6974,
+              aDayCalorie: (6974 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 8,
-            date: 9,
-            step: 8460,
-            aDayCalorie: (8460 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 8,
+              date: 9,
+              step: 8460,
+              aDayCalorie: (8460 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 9,
-            date: 10,
-            step: 6539,
-            aDayCalorie: (6539 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 9,
+              date: 10,
+              step: 6539,
+              aDayCalorie: (6539 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 10,
-            date: 11,
-            step: 7187,
-            aDayCalorie: (7187 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 10,
+              date: 11,
+              step: 7187,
+              aDayCalorie: (7187 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 11,
-            date: 12,
-            step: 11763,
-            aDayCalorie: (11763 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 11,
+              date: 12,
+              step: 11763,
+              aDayCalorie: (11763 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 12,
-            date: 13,
-            step: 14322,
-            aDayCalorie: (14322 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 12,
+              date: 13,
+              step: 14322,
+              aDayCalorie: (14322 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 13,
-            date: 14,
-            step: 881,
-            aDayCalorie: (881 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 13,
+              date: 14,
+              step: 881,
+              aDayCalorie: (881 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 14,
-            date: 15,
-            step: 6517,
-            aDayCalorie: (6571 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 14,
+              date: 15,
+              step: 6517,
+              aDayCalorie: (6571 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 15,
-            date: 16,
-            step: 1946,
-            aDayCalorie: (1946 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 15,
+              date: 16,
+              step: 1946,
+              aDayCalorie: (1946 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 16,
-            date: 17,
-            step: 8185,
-            aDayCalorie: (8185 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 16,
+              date: 17,
+              step: 8185,
+              aDayCalorie: (8185 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 17,
-            date: 18,
-            step: 13227,
-            aDayCalorie: (13227 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 17,
+              date: 18,
+              step: 13227,
+              aDayCalorie: (13227 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 18,
-            date: 19,
-            step: 411,
-            aDayCalorie: (411 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 18,
+              date: 19,
+              step: 411,
+              aDayCalorie: (411 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 19,
-            date: 20,
-            step: 9185,
-            aDayCalorie: (9185 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 19,
+              date: 20,
+              step: 9185,
+              aDayCalorie: (9185 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 20,
-            date: 21,
-            step: 3340,
-            aDayCalorie: (3340 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 20,
+              date: 21,
+              step: 3340,
+              aDayCalorie: (3340 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 21,
-            date: 22,
-            step: 1201,
-            aDayCalorie: (1201 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 21,
+              date: 22,
+              step: 1201,
+              aDayCalorie: (1201 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 22,
-            date: 23,
-            step: 9604,
-            aDayCalorie: (9604 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 22,
+              date: 23,
+              step: 9604,
+              aDayCalorie: (9604 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 23,
-            date: 24,
-            step: 8552,
-            aDayCalorie: (8552 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 23,
+              date: 24,
+              step: 8552,
+              aDayCalorie: (8552 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 24,
-            date: 25,
-            step: 307,
-            aDayCalorie: (307 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 24,
+              date: 25,
+              step: 307,
+              aDayCalorie: (307 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 25,
-            date: 26,
-            step: 8455,
-            aDayCalorie: (8455 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 25,
+              date: 26,
+              step: 8455,
+              aDayCalorie: (8455 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 26,
-            date: 27,
-            step: 9560,
-            aDayCalorie: (9560 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 26,
+              date: 27,
+              step: 9560,
+              aDayCalorie: (9560 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 27,
-            date: 28,
-            step: 9376,
-            aDayCalorie: (9376 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 27,
+              date: 28,
+              step: 9376,
+              aDayCalorie: (9376 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 28,
-            date: 29,
-            step: 3111,
-            aDayCalorie: (3111 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 28,
+              date: 29,
+              step: 3111,
+              aDayCalorie: (3111 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           },
 
-          {
-            index: 29,
-            date: 30,
-            step: 8700,
-            aDayCalorie: (8700 / 100 * 3.5).toFixed(3),
-            basalMetabolism: 1530
+            {
+              index: 29,
+              date: 30,
+              step: 8700,
+              aDayCalorie: (8700 / 100 * 3.5).toFixed(3),
+              basalMetabolism: 1530
           }
         ]
 
-      },
-        
+        },
       };
-    },
-      
-    
-
-    computed: {
-      num: function() {
-        return Math.floor(Math.random() * 30)
-      },
-      /*candrink: function(){
-        return sdata.id[num].aDayCalorie / 140
-      }
-      */
     },
     components: {
       Card,
